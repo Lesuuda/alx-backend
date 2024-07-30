c841f04008c49ea51b5a73994d573c9a4f0f7e62
lesuuda <lesuudaljamani@gmail.com>
2024-07-30 23:00:29 +0300

    core: tried translations but failed

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""basic flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from flask_babel import gettext as _
+
+
+class Config:
+    """configuration class for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "fr"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+def get_locale():
+    """gets local language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.context_processor
+def inject_translations():
+    return dict(_=_)
+
+
+@app.route('/')
+def home():
+    """renders a template 1-index.html"""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
